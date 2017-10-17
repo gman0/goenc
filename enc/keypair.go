9eb892039a4b6df0cc2de2e6cbe9abad5c29f6f5
gman <gman@codefreax.org>
2017-10-17 16:11:25 +0300

    keypair.go: generate PEM block in separate functions; added other functions

--- enc/keypair.go
+++ enc/keypair.go
@@ -5,17 +5,93 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
+	"fmt"
 )
 
+const (
+	PrivateKeyType = "RSA PRIVATE KEY"
+	PublicKeyType  = "PUBLIC KEY"
+	KeyLength      = 2048
+)
+
+type RSAPrivateKey struct {
+	*rsa.PrivateKey
+}
+
+func (key *RSAPrivateKey) ToPEM() []byte {
+	der := x509.MarshalPKCS1PrivateKey(key.PrivateKey)
+	block := pem.Block{
+		Type:    PrivateKeyType,
+		Headers: nil,
+		Bytes:   der,
+	}
+
+	return pem.EncodeToMemory(&block)
+}
+
+func ParsePrivateKeyFromPEM(data []byte) (RSAPrivateKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return RSAPrivateKey{}, errors.New("Error while decoding a PEM private key")
+	}
+
+	if block.Type != PrivateKeyType {
+		return RSAPrivateKey{}, errors.New(fmt.Sprintf(
+			"Cannot parse PEM private key of unsupported type %s", block.Type))
+	}
+
+	if key, err := x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
+		return RSAPrivateKey{}, err
+	} else {
+		return RSAPrivateKey{key}, nil
+	}
+}
+
+type RSAPublicKey struct {
+	*rsa.PublicKey
+}
+
+func (key *RSAPublicKey) ToPEM() ([]byte, error) {
+	der, err := x509.MarshalPKIXPublicKey(key.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	block := pem.Block{
+		Type:    PublicKeyType,
+		Headers: nil,
+		Bytes:   der,
+	}
+
+	return pem.EncodeToMemory(&block), nil
+}
+
+func ParsePublicKeyFromPEM(data []byte) (RSAPublicKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return RSAPublicKey{}, errors.New("Error while decoding a PEM public key")
+	}
+
+	if block.Type != PublicKeyType {
+		return RSAPublicKey{}, errors.New(fmt.Sprintf(
+			"Cannot parse PEM public key of unsupported type %s", block.Type))
+	}
+
+	if key, err := x509.ParsePKIXPublicKey(block.Bytes); err != nil {
+		return RSAPublicKey{}, err
+	} else {
+		return RSAPublicKey{key.(*rsa.PublicKey)}, nil
+	}
+}
+
 type KeyPair struct {
-	Public  []byte
-	Private []byte
+	Public  RSAPublicKey
+	Private RSAPrivateKey
 }
 
 func GenerateKeyPair() KeyPair {
-	// Private key generation
-
-	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	priv, err := rsa.GenerateKey(rand.Reader, KeyLength)
 	if err != nil {
 		panic(err)
 	}
@@ -25,30 +101,8 @@ func GenerateKeyPair() KeyPair {
 		panic(err)
 	}
 
-	privDer := x509.MarshalPKCS1PrivateKey(priv)
-	privBlk := pem.Block{
-		Type:    "RSA PRIVATE KEY",
-		Headers: nil,
-		Bytes:   privDer,
-	}
-
-	// Public key generation
-
-	pub := priv.PublicKey
-
-	pubDer, err := x509.MarshalPKIXPublicKey(&pub)
-	if err != nil {
-		panic(err)
-	}
-
-	pubBlk := pem.Block{
-		Type:    "PUBLIC KEY",
-		Headers: nil,
-		Bytes:   pubDer,
-	}
-
 	return KeyPair{
-		Private: pem.EncodeToMemory(&privBlk),
-		Public:  pem.EncodeToMemory(&pubBlk),
+		Private: RSAPrivateKey{priv},
+		Public:  RSAPublicKey{&priv.PublicKey},
 	}
 }
