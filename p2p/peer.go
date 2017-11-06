ab138ee7911577fd3063a275f55fdaf0e9f4d800
gman <gman@codefreax.org>
2017-11-06 14:55:50 +0200

    p2p/peer: added ConnEnc, ConnDec to the Peer struct

--- p2p/peer.go
+++ p2p/peer.go
@@ -1,12 +1,23 @@
 package p2p
 
 import (
+	"encoding/gob"
 	"github.com/gman0/goenc/enc"
 	"net"
 )
 
 type Peer struct {
-	Public enc.RSAPublicKey
-	Sk     *[enc.SymKeyLength]byte
-	Conn   net.Conn
+	Public  enc.RSAPublicKey
+	Sk      *[enc.SymKeyLength]byte
+	Conn    net.Conn
+	ConnEnc *gob.Encoder
+	ConnDec *gob.Decoder
+}
+
+func (p *Peer) Send(val interface{}) error {
+	return p.ConnEnc.Encode(val)
+}
+
+func (p *Peer) Recv(val interface{}) error {
+	return p.ConnDec.Decode(val)
 }
