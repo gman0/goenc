249d2c48916a928e5b6d244bfb501e1b0217fdc4
gman <gman@codefreax.org>
2017-11-06 19:11:12 +0200

    proto: the protocol should be now finished

--- proto.go
+++ proto.go
@@ -1,15 +1,65 @@
 package goenc
 
 import (
+	"fmt"
 	"github.com/gman0/goenc/enc"
 	"github.com/gman0/goenc/p2p"
-
-	"fmt"
 )
+
+/*
+type AwaitingRequest struct {
+	Req    *Request
+	Answer chan bool
+}
+
+var (
+	nextReqId = 0
+	reqs  = make(map[int]*Request)
+)
+*/
 
 // Called from server side
 func HandleClientConnection(p *p2p.Peer, kp *enc.KeyPair) error {
+	// Send my public key
+	if err := SendPubKey(p, kp); err != nil {
+		return err
+	}
+
 	// Receive client's public key
+	if err := RecvPubKey(p); err != nil {
+		return err
+	}
+
+	if err := RecvAndDecryptSymKey(p, kp); err != nil {
+		return err
+	}
+
+	req, err := RecvAndDecryptRequest(p)
+	if err != nil {
+		return err
+	}
+	fmt.Println(req)
+
+	hmac, err := RecvFile("/tmp/recv", req.Size, p)
+	if err != nil {
+		return err
+	}
+
+	var sig []byte
+	if err = p.Recv(&sig); err != nil {
+		return err
+	}
+
+	if err = p.Public.VerifySignature(hmac, sig); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Called from client side
+func HandleServerConnection(filepath string, p *p2p.Peer, kp *enc.KeyPair) error {
+	// Receive server's public key
 	if err := RecvPubKey(p); err != nil {
 		return err
 	}
@@ -20,34 +70,33 @@ func HandleClientConnection(p *p2p.Peer, kp *enc.KeyPair) error {
 	}
 
 	// Send the encrypted symmetric key
-	symKey, err := GenerateAndSendSymKey(p)
+	if err := GenerateAndSendSymKey(p); err != nil {
+		return err
+	}
+
+	req, err := NewRequest(filepath)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(symKey)
-
-	return nil
-}
-
-// Called from client side
-func HandleServerConnection(filepath string, p *p2p.Peer, kp *enc.KeyPair) error {
-	// Send my public key
-	if err := SendPubKey(p, kp); err != nil {
+	fmt.Println(req)
+	if err = req.EncryptAndSend(p); err != nil {
 		return err
 	}
 
-	// Receive server's public key
-	if err := RecvPubKey(p); err != nil {
-		return err
-	}
-
-	symKey, err := RecvAndDecryptSymKey(p, kp)
+	hmac, err := SendFile(filepath, p)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(symKey)
+	sig, err := kp.Private.Sign(hmac)
+	if err != nil {
+		return err
+	}
+
+	if err = p.Send(sig); err != nil {
+		return err
+	}
 
 	return nil
 }
